cc68b3edff8300c6af5e4568f5cf5d8f2921a022
abelhavalos <51245849+abelhavalos@users.noreply.github.com>
2026-03-05 21:59:44 -0800

    Update messages.js

--- scripts/messages.js
+++ scripts/messages.js
@@ -311,7 +311,7 @@ function startPolling() {
   // Poll every 5 seconds
   pollingInterval = setInterval(() => {
     loadMessagesOnce(false);
-  }, 5000);
+  }, 200);
 }
 
 /****************************************************
