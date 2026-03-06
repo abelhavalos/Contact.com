1da9f421c5057f7c876cf731b2f6df8b1187607b
abelhavalos <51245849+abelhavalos@users.noreply.github.com>
2026-03-05 23:01:54 -0800

    Update messages.js

--- scripts/messages.js
+++ scripts/messages.js
@@ -304,7 +304,7 @@ function startPolling() {
     if (activeConversationId) {
        loadMessagesOnce(false);
     }
-  }, 4000); 
+  }, 2000); 
 }
 
 /****************************************************
