aca08b4fd5b78c9fb3dc881891d6802c56398144
abelhavalos <51245849+abelhavalos@users.noreply.github.com>
2026-03-14 23:09:57 -0700

    Update messages.js

--- scripts/messages.js
+++ scripts/messages.js
@@ -98,7 +98,6 @@ function loadNavbar() {
     <div class="nav-links">
       <a href="dashboard.html">Dashboard</a>
       <a href="communities.html">Communities</a>
-      <a href="events.html">Events</a>
       <a href="contacts.html">Contacts</a>
       <a href="profile.html">Profile</a>
       <a href="#" onclick="logout()">Logout</a>
@@ -109,7 +108,6 @@ function loadNavbar() {
     mobileMenu.innerHTML = `
       <a href="dashboard.html">Dashboard</a>
       <a href="communities.html">Communities</a>
-      <a href="events.html">Events</a>
       <a href="contacts.html">Contacts</a>
       <a href="profile.html">Profile</a>
       <a href="#" onclick="logout()">Logout</a>`;
